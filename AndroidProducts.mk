4d651f74ff633be6ac8dfe65bd3f4ebd3ee3f80b
dabao1955 <195328750@qq.com>
2023-07-10 15:22:25 +0800

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ossi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ossi-user \
+    omni_ossi-userdebug \
+    omni_ossi-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for oplus ossi (ossi)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
