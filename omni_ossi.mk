1259946ab3a85d9097e3ebfc854469a059bdd2db
dabao1955 <79307765+dabao1955@users.noreply.github.com>
2023-07-10 18:18:01 +0800

    Update omni_ossi.mk

--- omni_ossi.mk
+++ omni_ossi.mk
@@ -9,9 +9,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from ossi device
 $(call inherit-product, device/oplus/ossi/device.mk)
 
